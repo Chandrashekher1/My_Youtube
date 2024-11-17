3d2c4d35402c121f29fced962dd2fcd132906d39
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-11-17 17:34:20 +0530

    Add Router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
+import { Provider } from "react-redux";
 import "./App.css"
 import Body from "./Components/Body";
 import Header from "./Components/Header";
+import store from "./utils/store";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import MainContainer from "./Components/MainContainer";
+import WatchPage from "./Components/WatchPage";
+
+const appRouter = createBrowserRouter([{
+  path:"/",
+  element:<Body/>,
+  children :[
+    {
+      path:"/",
+      element:<MainContainer/>
+    },
+    {
+      path:"/watch",
+      element:<WatchPage/>
+    }
+
+  ]
+}])
+
 function App() {
   return (
-    
-    <div>
-      <Header/>
-      <Body/>
-      
+    <Provider store={store}>
+      <div>
+        <Header/>
+        <RouterProvider router={appRouter}/>    
     </div>
+    </Provider>
+    
   );
 }
 

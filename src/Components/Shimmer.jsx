ff30fef970b1aeccbd2791025e9b389f1c2a45fb
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-11-17 17:36:44 +0530

    Add Shimmer component

---
+++ src/Components/Shimmer.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Shimmer = () => {
+  return (
+    <div className='flex flex-wrap mx-4'>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+        <div className='w-72 h-40 m-4 bg-gray-400'></div>
+
+    
+    </div>
+  )
+}
+
+export default Shimmer

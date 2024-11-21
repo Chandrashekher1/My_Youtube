03ea8f2a3150fdac089187823d238f0a3ce4312f
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-11-21 23:16:49 +0530

    Add LivveChat Component

---
+++ src/Components/LiveChat.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ChatMessage from './ChatMessage'
+
+const LiveChat = () => {
+  return (
+    <div className='ml-2 rounded-lg p-2 w-full h-[500px] border border-black '>
+        <ChatMessage name="Chandu" message ="Lorem he na"  />
+        <ChatMessage name="Chandu" message ="Lorem he na"  />
+        <ChatMessage name="Chandu" message ="Lorem he na"  />
+        <ChatMessage name="Chandu" message ="Lorem he na"  />
+        <ChatMessage name="Chandu" message ="Lorem he na"  />
+        <ChatMessage name="Chandu" message ="Lorem he na"  />
+        <ChatMessage name="Chandu" message ="Lorem he na"  />
+        <ChatMessage name="Chandu" message ="Lorem he na"  />
+
+    </div>
+  )
+}
+
+export default LiveChat
